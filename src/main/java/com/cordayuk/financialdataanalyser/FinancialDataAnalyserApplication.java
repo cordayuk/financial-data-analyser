ebc56b7262cbb1f5af0946bc457972ccad047d82
Chris Corday <corday@gmail.com>
2020-04-28 17:38:11 +0100

    Initial commit

---
+++ src/main/java/com/cordayuk/financialdataanalyser/FinancialDataAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.cordayuk.financialdataanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDataAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialDataAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cordayuk/financialdataanalyser/FinancialDataAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordayuk.financialdataanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDataAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
